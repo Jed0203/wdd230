aa292f25cde71271f0e495960c10f411d184243b
jed0203 <cha19057@byui.edu>
2023-11-04 13:16:07 -0600

    Add last modified feature

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,12 @@ const currentYear = currentDate.getFullYear();
 
 let year = document.getElementById('currentYear');
 year.innerHTML = currentYear;
+
+// Get the last modified date and time
+const lastModified = document.lastModified;
+
+// Display the last modified date and time on the page
+const displayElement = document.getElementById('lastModified');
+if (displayElement) {
+    displayElement.textContent = "Last modified: " + lastModified;
+}
