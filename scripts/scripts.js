0a34f16f294d95e3daa7dec528fecfc1412651dc
jed0203 <cha19057@byui.edu>
2023-11-09 00:03:37 -0700

    Added dark mode

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,3 +12,12 @@ const displayElement = document.getElementById('lastModified');
 if (displayElement) {
     displayElement.textContent = "Last modified: " + lastModified;
 }
+
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	body.classList.toggle('dark-mode');
+	main.classList.toggle('dark-mode');
+});
