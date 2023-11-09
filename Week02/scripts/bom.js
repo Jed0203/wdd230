b14a474f7e53408581087c0ce322b90335816387
jed0203 <cha19057@byui.edu>
2023-11-08 19:22:47 -0700

    Added remove input value from input bar after clicking Add chapter

---
+++ Week02/scripts/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener("click", () => {
+    if (input.value) {
+      const li = document.createElement("li");
+      const deleteButton = document.createElement("button");
+  
+      newLi.textContent = input.value;
+      deleteButton.textContent = "❌";
+  
+      li.append(deleteButton);
+      list.append(li);
+  
+      deleteButton.addEventListener("click", () => {
+        list.removeChild(li);
+        input.focus();
+  
+        input.value = "";
+      });
+      // Clear the input field
+    input.value = "";
+    } else {
+      input.focus();
+    }
+  });
