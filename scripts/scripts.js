49243f7d13b54e9bed4aa95140acce4dbd3cbd9a
jed0203 <cha19057@byui.edu>
2023-11-10 11:41:26 -0700

    Tried fixed Hamburger menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,3 +21,12 @@ modeButton.addEventListener("click", () => {
 	body.classList.toggle('dark-mode');
 	main.classList.toggle('dark-mode');
 });
+
+// Store the selected elements that we are going to use. 
+const navigation = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hambutton.classList.toggle('open');
+});
