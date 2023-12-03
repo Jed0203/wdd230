d693442d441e4db5c80a07761213130b45a9acd7
jed0203 <cha19057@byui.edu>
2023-12-02 18:55:34 -0700

    Upload member json and scripts

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -54,3 +54,131 @@ function setTimestamp() {
   const formattedTimestamp = currentDate.toLocaleString();
 }
 setTimestamp();
+
+// Members Dinamically
+
+const cardContainer = document.querySelector("#card-container");
+const membersUrl = "https://jed0203.github.io/wdd230/chamber/data/member.json";
+
+async function getMembers() {
+  try {
+    const response = await fetch(membersUrl);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayMembers(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayMembers(membersData) {
+  membersData.companies.forEach((company) => {
+    const article = document.createElement("article");
+    article.classList.add("container");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = company.name;
+
+    const address = document.createElement("p");
+    address.textContent = company.address;
+
+    const phoneNumber = document.createElement("p");
+    phoneNumber.textContent = company.phoneNumber;
+
+    const website = document.createElement("a");
+    website.setAttribute("href", company.websiteURL);
+    website.textContent = company.websiteURL;
+    website.classList.add("memberLink");
+
+    const memberShipLevel = document.createElement("p");
+    memberShipLevel.textContent = `MemberShip Level: ${company.membershipLevel}`;
+
+    const img = document.createElement("img");
+    img.setAttribute("src", company.image);
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(address);
+    article.appendChild(phoneNumber);
+    article.appendChild(website);
+    article.appendChild(memberShipLevel);
+
+    cardContainer.appendChild(article);
+  });
+}
+
+if (cardContainer) {
+  getMembers();
+}
+
+/*SELECT THE VIEW*/
+
+const changeView = () => {
+  const selector = document.querySelector("#view");
+  const main = document.querySelector("#card-container");
+  const selectedValue = selector.value;
+  if (selectedValue === "column") {
+    main.classList.remove("grid");
+    main.classList.toggle("column");
+  } else if (selectedValue === "grid") {
+    main.classList.remove("column");
+    main.classList.toggle("grid");
+  }
+};
+
+const changeViewVar = document.querySelector("#view");
+if (changeViewVar) {
+  changeViewVar.addEventListener("change", changeView);
+}
+
+/*Display temperature */
+
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=23.84&lon=120.98&appid=76cd0cc1ffbcb7fcbc8193cf2c4758e5";
+
+async function apiFetch() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  displayResults(data);
+}
+
+function displayResults(data) {
+  currentTemp.textContent = `${data.main.temp} °F`;
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  captionDesc.textContent = data.weather[0].description;
+}
+
+apiFetch();
+
+async function getThreDayForecast() {
+  const url =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=23.84&lon=120.98&appid=76cd0cc1ffbcb7fcbc8193cf2c4758e5&units=imperial";
+  const response = await fetch(url);
+  const forecastData = await response.json();
+  displayForecast(forecastData);
+}
+
+getThreDayForecast();
+
+function displayForecast(forecastData) {
+  const forecastDiv = document.querySelector("#three-days-forecast");
+  forecastData.list.forEach((threeHour, i) => {
+    if ([9, 18, 36].includes(i)) {
+      const dayContainer = document.createElement("div");
+      dayContainer.classList.add("day-container");
+      const p = document.createElement("p");
+      p.textContent = threeHour.main.temp;
+      dayContainer.appendChild(p);
+      forecastDiv.appendChild(dayContainer);
+    }
+  });
+}
